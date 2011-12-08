ac9f8761feee777eeb0c95e8c0637c2082c1e4a9
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2011-12-08 10:57:50 -0200

    test-insmod: show the path libkmod is using

--- test/test-insmod.c
+++ test/test-insmod.c
@@ -34,7 +34,8 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Trying insmod '%s' (%s)\n", kmod_module_get_name(mod), path);
+	printf("Trying insmod '%s' (%s)\n", kmod_module_get_name(mod),
+						kmod_module_get_path(mod));
 	err = kmod_module_insert_module(mod, 0);
 	if (err < 0) {
 		fprintf(stderr, "%s\n", strerror(-err));
