cb451f35d9fe25ec1dee0628f8af23f022358f6b
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2011-12-12 18:24:35 -0200

    Change licenses
    libkmod is under LGPL 2.1 or later
    tools/* are under GPL

--- libkmod/libkmod-index.h
+++ libkmod/libkmod-index.h
@@ -1,12 +1,12 @@
 /*
  * libkmod - interface to kernel module operations
  *
- * Copyright (C) 2008  Alan Jenkins <alan.christopher.jenkins@googlemail.com>
  * Copyright (C) 2011  ProFUSION embedded systems
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation version 2.1.
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
