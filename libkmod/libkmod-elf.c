b20dc170681cab9a8c5b6d26d0687390fdd94359
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2011-12-20 03:12:35 -0200

    Remove unneeded reference to last string

--- libkmod/libkmod-elf.c
+++ libkmod/libkmod-elf.c
@@ -420,18 +420,11 @@ int kmod_elf_get_strings(const struct kmod_elf *elf, const char *section, char *
 	if (size <= 1)
 		return 0;
 
-	last = 0;
 	for (i = 0, count = 0; i < size; i++) {
 		if (strings[i] != '\0')
 			continue;
 
-		if (last == i) {
-			last = i + 1;
-			continue;
-		}
-
 		count++;
-		last = i + 1;
 	}
 
 	if (strings[i - 1] != '\0')
