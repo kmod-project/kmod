075f4ea0e247ceee234ed05bc04adecfeb0cf3af
W. Trevor King <wking@tremily.us>
2012-10-19 00:08:00 -0400

    python: Add missing copyright blurbs.

--- libkmod/python/setup.py
+++ libkmod/python/setup.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2012 Red Hat, Inc. All rights reserved.
+#               2012 W. Trevor King
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU Lesser General Public License v.2.1.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 from distutils.core import setup
 from distutils.extension import Extension as _Extension
 import os as _os
