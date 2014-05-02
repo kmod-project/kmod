3f8dd30a7605f7a14567dad78c619f846d0d5172
Lucas De Marchi <lucas.demarchi@intel.com>
2014-05-02 12:56:46 -0300

    testsuite: Add module playground dir

---
+++ testsuite/module-playground/moduleC.c
@@ -0,0 +1,24 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+
+#include "printmodule.h"
+
+static int __init test_module_init(void)
+{
+	printC();
+	printB();
+	return 0;
+}
+module_init(test_module_init);
+
+void printC(void)
+{
+	pr_warn("Hello, world C\n");
+}
+EXPORT_SYMBOL(printC);
+
+MODULE_AUTHOR("Lucas De Marchi <lucas.demarchi@intel.com>");
+MODULE_LICENSE("GPL");
---
+++ testsuite/module-playground/printmodule.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void printA(void);
+void printB(void);
+void printC(void);
