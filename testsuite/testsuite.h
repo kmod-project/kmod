b77251f08d3c1acc28f1f453da5e3971d50e0bc2
Lucas De Marchi <lucas.demarchi@intel.com>
2021-02-12 01:45:23 -0800

    testsuite: allow to re-use single function for tests
    Add a new DEFINE_TEST_WITH_FUNC() that accepts the function
    alongside the test name. This will allow us to share a single function
    for different tests.

--- testsuite/testsuite.h
+++ testsuite/testsuite.h
@@ -140,13 +140,15 @@ int test_run(const struct test *t);
 
 
 /* Test definitions */
-#define DEFINE_TEST(_name, ...) \
+#define DEFINE_TEST_WITH_FUNC(_name, _func, ...) \
 	static const struct test UNIQ(s##_name) \
 	__attribute__((used, section("kmod_tests"), aligned(8))) = { \
 		.name = #_name, \
-		.func = _name, \
+		.func = _func, \
 		## __VA_ARGS__ \
 	};
+
+#define DEFINE_TEST(_name, ...) DEFINE_TEST_WITH_FUNC(_name, _name, __VA_ARGS__)
 
 #define TESTSUITE_MAIN() \
 	extern struct test __start_kmod_tests[] __attribute__((weak, visibility("hidden")));	\
