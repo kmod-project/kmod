2ab4fbcb77aec709261a07d51c3175153c2a4dca
Lucas De Marchi <lucas.de.marchi@gmail.com>
2022-06-03 14:50:42 -0700

    module-playground: Add debugfs entry in mod-simple
    Add a debugfs file in mod-simple for manual tests: insert the module and
    open the file to have its refcount increased.
    
    Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

--- testsuite/module-playground/mod-simple.c
+++ testsuite/module-playground/mod-simple.c
@@ -1,16 +1,32 @@
+#include <linux/debugfs.h>
 #include <linux/init.h>
 #include <linux/module.h>
 
+static struct dentry *debugfs_dir;
+
+static int test_show(struct seq_file *s, void *data)
+{
+	seq_puts(s, "test");
+	return 0;
+}
+
+DEFINE_SHOW_ATTRIBUTE(test);
+
 static int __init test_module_init(void)
 {
+	debugfs_dir = debugfs_create_dir(KBUILD_MODNAME, NULL);
+	debugfs_create_file("test", 0444, debugfs_dir, NULL, &test_fops);
+
 	return 0;
 }
 
 static void test_module_exit(void)
 {
+	debugfs_remove_recursive(debugfs_dir);
 }
+
 module_init(test_module_init);
 module_exit(test_module_exit);
 
 MODULE_AUTHOR("Lucas De Marchi <lucas.demarchi@intel.com>");
-MODULE_LICENSE("LGPL");
+MODULE_LICENSE("GPL");
