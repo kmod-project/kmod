9d45fa79e69955e1ccbe22edce05375832d109bd
Lucas De Marchi <lucas.de.marchi@gmail.com>
2024-07-09 01:08:56 -0500

    libkmod: Move xz-related functions to separate file
    Move xz-related function to a separate file so it's easier to isolate
    the dependency on each decompression library.
    
    Declare struct kmod_file in a shared libkmod-internal-file.h that will
    be included only by sources implementing kmod_file decompression
    routines.
    
    Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

---
+++ libkmod/libkmod-internal-file.h
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/*
+ * Copyright © 2024 Intel Corporation
+ */
+
+#include <errno.h>
+
+#include <libkmod/libkmod-internal.h>
+
+struct kmod_ctx;
+struct kmod_elf;
+
+struct kmod_file {
+	int fd;
+	enum kmod_file_compression_type compression;
+	off_t size;
+	void *memory;
+	int (*load)(struct kmod_file *file);
+	const struct kmod_ctx *ctx;
+	struct kmod_elf *elf;
+};
+
+#ifdef ENABLE_XZ
+int kmod_file_load_xz(struct kmod_file *file);
+#else
+static inline int kmod_file_load_xz(struct kmod_file *file) { return -ENOSYS; }
+#endif
