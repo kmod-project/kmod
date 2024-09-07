a10cfe3436c40010386f5b4fd36b5dba2372ab33
Emil Velikov <emil.l.velikov@gmail.com>
2024-09-07 01:15:04 +0100

    Remove _must_check_ and -Wno-unused-result
    We don't have any instances that would require the latter warning to be
    suppressed. In addition, the former detects when one (void) casts the
    result of the function call - again zero instances in tree.
    
    So let's remove them both.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- shared/util.h
+++ shared/util.h
@@ -24,23 +24,23 @@ void *memdup(const void *p, size_t n) __attribute__((nonnull(1)));
 /* ************************************************************************ */
 #define KMOD_EXTENSION_UNCOMPRESSED ".ko"
 
-int alias_normalize(const char *alias, char buf[static PATH_MAX], size_t *len) _must_check_ __attribute__((nonnull(1,2)));
-int underscores(char *s) _must_check_;
+int alias_normalize(const char *alias, char buf[static PATH_MAX], size_t *len) __attribute__((nonnull(1,2)));
+int underscores(char *s);
 char *modname_normalize(const char *modname, char buf[static PATH_MAX], size_t *len) __attribute__((nonnull(1, 2)));
 char *path_to_modname(const char *path, char buf[static PATH_MAX], size_t *len) __attribute__((nonnull(2)));
 bool path_ends_with_kmod_ext(const char *path, size_t len) __attribute__((nonnull(1)));
 
 /* read-like and fread-like functions                                       */
 /* ************************************************************************ */
-ssize_t read_str_safe(int fd, char *buf, size_t buflen) _must_check_ __attribute__((nonnull(2)));
+ssize_t read_str_safe(int fd, char *buf, size_t buflen) __attribute__((nonnull(2)));
 ssize_t write_str_safe(int fd, const char *buf, size_t buflen) __attribute__((nonnull(2)));
-int read_str_long(int fd, long *value, int base) _must_check_ __attribute__((nonnull(2)));
-int read_str_ulong(int fd, unsigned long *value, int base) _must_check_ __attribute__((nonnull(2)));
+int read_str_long(int fd, long *value, int base) __attribute__((nonnull(2)));
+int read_str_ulong(int fd, unsigned long *value, int base) __attribute__((nonnull(2)));
 char *freadline_wrapped(FILE *fp, unsigned int *linenum) __attribute__((nonnull(1)));
 
 /* path handling functions                                                  */
 /* ************************************************************************ */
-char *path_make_absolute_cwd(const char *p) _must_check_ __attribute__((nonnull(1)));
+char *path_make_absolute_cwd(const char *p) __attribute__((nonnull(1)));
 int mkdir_p(const char *path, int len, mode_t mode);
 int mkdir_parents(const char *path, mode_t mode);
 unsigned long long stat_mstamp(const struct stat *st);
