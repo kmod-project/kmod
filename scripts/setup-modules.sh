aa9f6d89e7e226abc79978d5e7f69e27501bad45
Lucas De Marchi <lucas.de.marchi@gmail.com>
2024-09-11 08:51:23 -0500

    build: Fix KDIR again
    KDIR is not related to the what we configure in kmod's build. It's only
    used in kmod to locate where the distro's kernel source/headers is,
    which may be different from what we are configuring the (under
    development) kmod with.
    
    Remove the setting from meson/autotools and figure it out inside the
    module-playground Makefile what should be used. For advanced use cases,
    KDIR= can be passed to override the location.
    
    For our own tests, which includes testing with a different module_directory,
    scripts/setup-rootfs.sh will copy the module to the desired location
    according to the map defined in the script.
    
    Fixes: 27ff72732666 ("testsuite: correct the default KDIR")
    Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
    Link: https://github.com/kmod-project/kmod/pull/125

--- scripts/setup-modules.sh
+++ scripts/setup-modules.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 SRCDIR=$1
 BUILDDIR=$2
 MODULE_PLAYGROUND=$3
-# shellcheck disable=SC2034 # used by the Makefile
-export MODULE_DIRECTORY=$4
 
 # TODO: meson allows only out of tree builds
 if test "$SRCDIR" != "$BUILDDIR"; then
