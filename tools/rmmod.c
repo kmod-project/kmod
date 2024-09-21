kmod-project/kmod

commit a4bd1441e5c7e8903a9ae065801d4f06f13d06e9

Remove "rmmod -w" documentation and getopt entry
This patch removes the cmdopts declaration and the documentation. They
were leftover from the -w removal.

tools/rmmod.c
@@ -40,7 +40,6 @@ static const struct option cmdopts[] = {
 	{"syslog", no_argument, 0, 's'},
 	{"verbose", no_argument, 0, 'v'},
 	{"version", no_argument, 0, 'V'},
-	{"wait", no_argument, 0, 'w'},
 	{"help", no_argument, 0, 'h'},
 	{NULL, 0, 0, 0}
 };

man/rmmod.xml
@@ -39,7 +39,6 @@
     <cmdsynopsis>
       <command>rmmod</command>
       <arg><option>-f</option></arg>
-      <arg><option>-w</option></arg>
       <arg><option>-s</option></arg>
       <arg><option>-v</option></arg>
       <arg><replaceable>modulename</replaceable></arg>
@@ -90,22 +89,6 @@
             (see <citerefentry>
               <refentrytitle>lsmod</refentrytitle><manvolnum>8</manvolnum>
             </citerefentry>).
-          </para>
-        </listitem>
-      </varlistentry>
-      <varlistentry>
-        <term><option>-w</option> <option>--wait</option>
-        </term>
-        <listitem>
-          <para>
-            Normally, <command>rmmod</command> will refuse to unload modules
-            which are in use.  With this option, <command>rmmod</command> will
-            isolate the module, and wait until the module is no longer used.
-            Nothing new will be able to use the module, but it's up to you to
-            make sure the current users eventually finish with it.  See
-            <citerefentry>
-              <refentrytitle>lsmod</refentrytitle><manvolnum>8</manvolnum>
-            </citerefentry>) for information on usage counts.
           </para>
         </listitem>
       </varlistentry>

commit 519621b7e2cdfe9513ba52a63bd9a40b3ddd0839

tools/rmmod: remove unused -w shortopt
The (long)option and handling was removed some 10 years ago, yet the
shortopt remained... oops

Fixes: a4bd144 ("Remove "rmmod -w" documentation and getopt entry")
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Link: https://github.com/kmod-project/kmod/pull/138
Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

tools/rmmod.c
@@ -24,7 +24,7 @@
 static int verbose = DEFAULT_VERBOSE;
 static int use_syslog;
 
-static const char cmdopts_s[] = "fsvVwh";
+static const char cmdopts_s[] = "fsvVh";
 static const struct option cmdopts[] = {
 	// clang-format off
 	{ "force", no_argument, 0, 'f' },