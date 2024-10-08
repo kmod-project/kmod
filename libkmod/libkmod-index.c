e79bba6b4af86216072b56382198da645a0906f4
Emil Velikov <emil.l.velikov@gmail.com>
2024-10-08 18:04:34 +0100

    libkmod: call fnmatch() only as needed
    Currently we'll call the function even when the patterns is an empty
    string.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Link: https://github.com/kmod-project/kmod/pull/174
    Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

--- libkmod/libkmod-index.c
+++ libkmod/libkmod-index.c
@@ -511,7 +511,7 @@ static void index_searchwild__all(struct index_node_f *node, int j, struct strbu
 		}
 	}
 
-	if (node->values) {
+	if (pushed && node->values) {
 		const char *s = strbuf_str(buf);
 
 		if (s != NULL && fnmatch(s, subkey, 0) == 0)
@@ -997,7 +997,7 @@ static void index_mm_searchwild_all(struct index_mm_node *node, int j, struct st
 		}
 	}
 
-	if (node->values.len > 0) {
+	if (pushed && node->values.len > 0) {
 		const char *s = strbuf_str(buf);
 
 		if (s != NULL && fnmatch(s, subkey, 0) == 0)
