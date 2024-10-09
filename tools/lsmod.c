kmod-project/kmod

commit 6313d40dd59fdfa286b839155487a9335b70cafd

tools/lsmod: add basic opts like rmmod
This allows for more consistent experience across tools, allows to see
the version, observe/debug if "Used by" reports -1, etc.

Also don't forget to update the man page :-)

Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Link: https://github.com/kmod-project/kmod/pull/138
Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

tools/lsmod.c
@@ -4,6 +4,7 @@
  */
 
 #include <errno.h>
+#include <getopt.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,30 +15,86 @@
 
 #include "kmod.h"
 
+static const char cmdopts_s[] = "svVh";
+static const struct option cmdopts[] = {
+	// clang-format off
+	{ "syslog", no_argument, 0, 's' },
+	{ "verbose", no_argument, 0, 'v' },
+	{ "version", no_argument, 0, 'V' },
+	{ "help", no_argument, 0, 'h' },
+	{ NULL, 0, 0, 0 },
+	// clang-format on
+};
+
+static void help(void)
+{
+	printf("Usage:\n"
+		"\t%s [options]\n"
+		"Options:\n"
+		"\t-s, --syslog      print to syslog, not stderr\n"
+		"\t-v, --verbose     enables more messages\n"
+		"\t-V, --version     show version\n"
+		"\t-h, --help        show this help\n",
+		program_invocation_short_name);
+}
+
 static int do_lsmod(int argc, char *argv[])
 {
-	struct kmod_ctx *ctx;
+	struct kmod_ctx *ctx = NULL;
 	const char *null_config = NULL;
 	struct kmod_list *list, *itr;
-	int err;
+	int verbose = LOG_ERR;
+	int use_syslog;
+	int err, r = 0;
 
-	if (argc != 1) {
-		fprintf(stderr, "Usage: %s\n", argv[0]);
-		return EXIT_FAILURE;
+	for (;;) {
+		int c, idx = 0;
+		c = getopt_long(argc, argv, cmdopts_s, cmdopts, &idx);
+		if (c == -1)
+			break;
+		switch (c) {
+		case 's':
+			use_syslog = 1;
+			break;
+		case 'v':
+			verbose++;
+			break;
+		case 'h':
+			help();
+			return EXIT_SUCCESS;
+		case 'V':
+			kmod_version();
+			return EXIT_SUCCESS;
+		case '?':
+			return EXIT_FAILURE;
+		default:
+			ERR("unexpected getopt_long() value '%c'.\n", c);
+			return EXIT_FAILURE;
+		}
+	}
+
+	log_open(use_syslog);
+
+	if (optind < argc) {
+		ERR("too many arguments provided.\n");
+		r = EXIT_FAILURE;
+		goto done;
 	}
 
 	ctx = kmod_new(NULL, &null_config);
-	if (ctx == NULL) {
-		fputs("Error: kmod_new() failed!\n", stderr);
-		return EXIT_FAILURE;
+	if (!ctx) {
+		ERR("kmod_new() failed!\n");
+		r = EXIT_FAILURE;
+		goto done;
 	}
+
+	log_setup_kmod_log(ctx, verbose);
 
 	err = kmod_module_new_from_loaded(ctx, &list);
 	if (err < 0) {
-		fprintf(stderr, "Error: could not get list of modules: %s\n",
-			strerror(-err));
-		kmod_unref(ctx);
-		return EXIT_FAILURE;
+		ERR("could not get list of modules: %s\n", strerror(-err));
+		r = EXIT_FAILURE;
+		goto done;
 	}
 
 	puts("Module                  Size  Used by");
@@ -70,9 +127,12 @@ static int do_lsmod(int argc, char *argv[])
 		kmod_module_unref(mod);
 	}
 	kmod_module_unref_list(list);
-	kmod_unref(ctx);
 
-	return EXIT_SUCCESS;
+done:
+	kmod_unref(ctx);
+	log_close();
+
+	return r == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
 const struct kmod_cmd kmod_cmd_compat_lsmod = {

man/lsmod.8.scd
@@ -5,12 +5,32 @@ LSMOD(8) "kmod" "lsmod"
 lsmod - Show the status of modules in the Linux Kernel
 
 # SYNOPSIS
-*lsmod*
+
+*lsmod* [_OPTIONS_]
 
 # DESCRIPTION
 
 *lsmod* is a trivial program which nicely formats the contents of the
 /proc/modules, showing what kernel modules are currently loaded.
+
+# OPTIONS
+
+*-s*
+*--syslog*
+	Send errors to syslog instead of standard error.
+
+*-v*
+*--verbose*
+	Print messages about what the program is doing. Usually *lsmod* prints
+	messages only if something goes wrong.
+
+*-V*
+*--version*
+	Show version of program and exit.
+
+*-h*
+*--help*
+	Print the help message and exit.
 
 # COPYRIGHT
 

commit 6186b909d76a10bc836cbbd37342283e80d86cb7

tools/lsmod: initialize use_syslog
Initialize the variable, otherwise we'll get (random) stack value if the
user hasn't provided --syslog. Reported by scan-build.

Fixes: 6313d40d ("tools/lsmod: add basic opts like rmmod")
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Link: https://github.com/kmod-project/kmod/pull/172
Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

tools/lsmod.c
@@ -44,7 +44,7 @@ static int do_lsmod(int argc, char *argv[])
 	const char *null_config = NULL;
 	struct kmod_list *list, *itr;
 	int verbose = LOG_ERR;
-	int use_syslog;
+	int use_syslog = 0;
 	int err, r = 0;
 
 	for (;;) {