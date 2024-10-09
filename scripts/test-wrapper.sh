3bbae94de573eca922618d39ab4f200e422b39ae
Emil Velikov <emil.l.velikov@gmail.com>
2024-10-09 16:26:25 +0100

    Add support for clang sanitizers
    By default clang uses static sanitizer libraries, which cause build and
    test-time failures. Swap for the shared libasan which resolves both
    cases.
    
    Note: meson tries to be helpful here, throwing a warning that we should
    use -D b_lundef=false which is incorrect in our case.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- scripts/test-wrapper.sh
+++ scripts/test-wrapper.sh
@@ -3,7 +3,14 @@
 set -euo pipefail
 
 if [[ ${ASAN_OPTIONS-} || ${UBSAN_OPTIONS-} || ${MSAN_OPTIONS-} ]]; then
-    LD_PRELOAD=$(gcc -print-file-name=libasan.so)
+    if [[ $CC == *gcc* ]]; then
+        LD_PRELOAD=$("$CC" -print-file-name=libasan.so)
+    elif [[ $CC == *clang* ]]; then
+        LD_PRELOAD=$("$CC" -print-file-name=libclang_rt.asan-x86_64.so)
+    else
+        echo "Unknown compiler CC=\"$CC\""
+        exit 1
+    fi
     export LD_PRELOAD
 fi
 
