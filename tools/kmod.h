kmod-project/kmod

commit b644b8d784d17405a5e6da541cc337f7fd07f90d

tools: add kmod_version() helper
Add a trivial helper that prints the version + features combo. I will be
adding another instance of those, so I'm aiming to keep the boilerplate
code to a minimum.

Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Link: https://github.com/kmod-project/kmod/pull/138
Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

tools/kmod.h
@@ -25,4 +25,10 @@ extern const struct kmod_cmd kmod_cmd_list;
 extern const struct kmod_cmd kmod_cmd_static_nodes;
 extern const struct kmod_cmd kmod_cmd_remove;
 
+static inline void kmod_version(void)
+{
+	puts(PACKAGE " version " VERSION);
+	puts(KMOD_FEATURES);
+}
+
 #include "log.h"

tools/depmod.c
@@ -3026,8 +3026,7 @@ static int do_depmod(int argc, char *argv[])
 			help();
 			return EXIT_SUCCESS;
 		case 'V':
-			puts(PACKAGE " version " VERSION);
-			puts(KMOD_FEATURES);
+			kmod_version();
 			return EXIT_SUCCESS;
 		case '?':
 			goto cmdline_failed;

tools/insmod.c
@@ -77,8 +77,7 @@ static int do_insmod(int argc, char *argv[])
 			help();
 			return EXIT_SUCCESS;
 		case 'V':
-			puts(PACKAGE " version " VERSION);
-			puts(KMOD_FEATURES);
+			kmod_version();
 			return EXIT_SUCCESS;
 		case '?':
 			return EXIT_FAILURE;

tools/kmod.c
@@ -97,8 +97,7 @@ static int handle_kmod_commands(int argc, char *argv[])
 			kmod_help(argc, argv);
 			return EXIT_SUCCESS;
 		case 'V':
-			puts(PACKAGE " version " VERSION);
-			puts(KMOD_FEATURES);
+			kmod_version();
 			return EXIT_SUCCESS;
 		case '?':
 			return EXIT_FAILURE;

tools/modinfo.c
@@ -423,8 +423,7 @@ static int do_modinfo(int argc, char *argv[])
 			help();
 			return EXIT_SUCCESS;
 		case 'V':
-			puts(PACKAGE " version " VERSION);
-			puts(KMOD_FEATURES);
+			kmod_version();
 			return EXIT_SUCCESS;
 		case '?':
 			return EXIT_FAILURE;

tools/modprobe.c
@@ -938,8 +938,7 @@ static int do_modprobe(int argc, char **orig_argv)
 			verbose++;
 			break;
 		case 'V':
-			puts(PACKAGE " version " VERSION);
-			puts(KMOD_FEATURES);
+			kmod_version();
 			err = 0;
 			goto done;
 		case 'h':

tools/rmmod.c
@@ -120,8 +120,7 @@ static int do_rmmod(int argc, char *argv[])
 			help();
 			return EXIT_SUCCESS;
 		case 'V':
-			puts(PACKAGE " version " VERSION);
-			puts(KMOD_FEATURES);
+			kmod_version();
 			return EXIT_SUCCESS;
 		case '?':
 			return EXIT_FAILURE;

commit 2224ec8b9b4a559ff23381d1e09bf38359c90c14

tools: add missing stdio.h include
Required for puts(), reported by clang-tidy.

Fixes: b644b8d7 ("tools: add kmod_version() helper")
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Link: https://github.com/kmod-project/kmod/pull/172
Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

tools/kmod.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include <stdio.h>
+
 #include <shared/macro.h>
 
 struct kmod_cmd {