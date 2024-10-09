f5b4ff82f45af668cb4d62043184829b1e9621cb
Emil Velikov <emil.l.velikov@gmail.com>
2024-10-09 16:26:25 +0100

    Add support for clang sanitizers
    By default clang uses static sanitizer libraries, which causes build and
    test-time failures. Swap for the shared libasan which resolves both.
    
    Note: meson tries to be helpful here, throwing a warning that we should
    use -D b_lundef=false which is incorrect in our case.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Link: https://github.com/kmod-project/kmod/pull/172
    Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

--- scripts/sanitizer-env.sh
+++ scripts/sanitizer-env.sh
@@ -2,7 +2,16 @@
 
 # set -euo pipefail # don't set these, since this script is sourced
 
-OUR_PRELOAD=$(gcc -print-file-name=libasan.so)
+if [[ ${CC-} == *gcc* ]]; then
+    OUR_PRELOAD=$("$CC" -print-file-name=libasan.so)
+elif [[ ${CC-} == *clang* ]]; then
+    OUR_PRELOAD=$("$CC" -print-file-name=libclang_rt.asan-x86_64.so)
+else
+    echo "Unknown compiler CC=\"${CC-}\" - gcc and clang are supported."
+    echo "Assuming \"gcc\", manually set the variable and retry if needed."
+    echo
+    OUR_PRELOAD=$(gcc -print-file-name=libasan.so)
+fi
 
 if test -n "$OUR_PRELOAD"; then
     # In some cases, like in Fedora, the file is a script which cannot be
