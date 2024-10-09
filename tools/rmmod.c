kmod-project/kmod

commit 24fe68dcc1b4147dd2550306749cfa6b1582fb81

tools/rmmod: make opt variables non-global
They're used locally within one function so declare them there.

Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Link: https://github.com/kmod-project/kmod/pull/138
Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

tools/rmmod.c
@@ -20,10 +20,6 @@
 
 #include "kmod.h"
 
-#define DEFAULT_VERBOSE LOG_ERR
-static int verbose = DEFAULT_VERBOSE;
-static int use_syslog;
-
 static const char cmdopts_s[] = "fsvVh";
 static const struct option cmdopts[] = {
 	// clang-format off
@@ -97,6 +93,8 @@ static int do_rmmod(int argc, char *argv[])
 {
 	struct kmod_ctx *ctx;
 	const char *null_config = NULL;
+	int verbose = LOG_ERR;
+	int use_syslog;
 	int flags = 0;
 	int i, err, r = 0;
 

commit c566ae7a34aeabb1e36ec9510eee2cd8cea54d1e

tools/rmmod: initialize use_syslog
Initialize the variable, otherwise we'll get (random) stack value if the
user hasn't provided --syslog. Reported by scan-build.

Fixes: 24fe68dc ("tools/rmmod: make opt variables non-global")
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Link: https://github.com/kmod-project/kmod/pull/172
Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

tools/rmmod.c
@@ -94,7 +94,7 @@ static int do_rmmod(int argc, char *argv[])
 	struct kmod_ctx *ctx;
 	const char *null_config = NULL;
 	int verbose = LOG_ERR;
-	int use_syslog;
+	int use_syslog = 0;
 	int flags = 0;
 	int i, err, r = 0;
 