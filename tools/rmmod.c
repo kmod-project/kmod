8feccfc21ddccd507daf45c899eefe55f0148190
Emil Velikov <emil.l.velikov@gmail.com>
2024-10-09 11:39:52 +0100

    man/rmmod: document (how) we handle list of modules
    Mention in the help and docs that rmmod operates on list of modules,
    alongside its current mode of operation.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Link: https://github.com/kmod-project/kmod/pull/177
    Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

--- tools/rmmod.c
+++ tools/rmmod.c
@@ -35,7 +35,7 @@ static const struct option cmdopts[] = {
 static void help(void)
 {
 	printf("Usage:\n"
-	       "\t%s [options] modulename ...\n"
+	       "\t%s [options] [list of modulenames]\n"
 	       "Options:\n"
 	       "\t-f, --force       DANGEROUS: forces a module unload and may\n"
 	       "\t                  crash your machine\n"
