cfd2485a75f0dc5b76fb68576cf0b3962a366510
Martin Wilck <martin_wilck@gmx.de>
2024-11-14 22:33:19 +0100

    testsuite: add unit test for incremental module addition
    This test uses the same module set as "big-01". But it simulates
    an incremental addition. The test directory contains modules.dep
    and other output files from a "previous" depmod run, where some
    modules (scsi_transport_sas, qla2xxx and their dependencies)
    had been missing. The test then runs "depmod -I -a" over this
    directory with the "previously missing" modules now present.
    The final results should be equal to those of the big-01 test.
    
    Signed-off-by: Martin Wilck <martin_wilck@gmx.de>

--- testsuite/test-depmod.c
+++ testsuite/test-depmod.c
@@ -400,4 +400,42 @@ DEFINE_TEST(depmod_test_big_01,
 		},
 	});
 
+#define TEST_BIG_01_INCREMENTAL_ROOTFS TESTSUITE_ROOTFS "test-depmod/big-01-incremental"
+static noreturn int depmod_test_big_01_incremental(const struct test *t)
+{
+	const char *progname = TOOLS_DIR "/depmod";
+	const char *const args[] = {
+		progname, "-e",
+		"-E",	  TEST_BIG_01_INCREMENTAL_ROOTFS "/lib/modules/5.3.18/symvers",
+		"-I",	  "-a",
+		NULL,
+	};
+
+	test_spawn_prog(progname, args);
+	exit(EXIT_FAILURE);
+}
+DEFINE_TEST(depmod_test_big_01_incremental,
+#if defined(KMOD_SYSCONFDIR_NOT_ETC)
+        .skip = true,
+#endif
+	.description = "check generating incremental dependencies the big-01 module set",
+	.config = {
+		[TC_UNAME_R] = "5.3.18",
+		[TC_ROOTFS] = TEST_BIG_01_INCREMENTAL_ROOTFS,
+	},
+	.output = {
+		.regex = true,
+		.files = (const struct keyval[]) {
+			{ TEST_BIG_01_INCREMENTAL_ROOTFS "/lib/modules/5.3.18/correct-modules.dep",
+			  TEST_BIG_01_INCREMENTAL_ROOTFS "/lib/modules/5.3.18/modules.dep" },
+			{ TEST_BIG_01_INCREMENTAL_ROOTFS "/lib/modules/5.3.18/correct-modules.dep.bin",
+			  TEST_BIG_01_INCREMENTAL_ROOTFS "/lib/modules/5.3.18/modules.dep.bin" },
+			{ TEST_BIG_01_INCREMENTAL_ROOTFS "/lib/modules/5.3.18/correct-modules.symbols",
+			  TEST_BIG_01_INCREMENTAL_ROOTFS "/lib/modules/5.3.18/modules.symbols" },
+			{ TEST_BIG_01_INCREMENTAL_ROOTFS "/lib/modules/5.3.18/correct-modules.symbols.bin",
+			  TEST_BIG_01_INCREMENTAL_ROOTFS "/lib/modules/5.3.18/modules.symbols.bin" },
+			{ }
+		},
+	});
+
 TESTSUITE_MAIN();
