54d28d0f732ce5173aaaba041d90b21a493407f9
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2024-11-27 11:09:12 -0500

    tools/insmod: move main logic into helpers
    Move code in do_insmod into two helpers: one to get the
    dirname ($root/MODULE_DIRECTORY/$kernel) and another to do the main
    logic.
    
    The first helper, get_module_dirname(), is also always called to
    figure the dirname path and to avoid providing NULL to the kmod_new()
    API. This is because kmod_new() always defaults to MODULE_DIRECTORY when
    the dirname path is empty, and won't work when MODULE_ALTERNATIVE_DIRECTORY
    will be used.
    
    This is in preparation to perform the same two steps again for
    the new MODULE_ALTERNATIVE_DIRECTORY option.
    
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>

--- tools/insmod.c
+++ tools/insmod.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/utsname.h>
 
 #include <shared/util.h>
 
@@ -58,16 +59,68 @@ static const char *mod_strerror(int err)
 	}
 }
 
-static int do_insmod(int argc, char *argv[])
+static int get_module_dirname(char *dirname_buf, size_t dirname_size,
+			      const char *module_directory)
+{
+	struct utsname u;
+	int n;
+
+	if (uname(&u) < 0)
+		return EXIT_FAILURE;
+
+	n = snprintf(dirname_buf, dirname_size, "%s/%s", module_directory,
+		     u.release);
+	if (n >= (int)dirname_size) {
+		ERR("bad directory %s/%s: path too long\n",
+		    module_directory, u.release);
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+static int _do_insmod(const char *dirname, const char *filename,
+		      unsigned int flags, char *opts, int verbose)
 {
 	struct kmod_ctx *ctx = NULL;
+	const char *null_config = NULL;
 	struct kmod_module *mod;
+	int r;
+
+	ctx = kmod_new(dirname, &null_config);
+	if (!ctx) {
+		ERR("kmod_new() failed!\n");
+		r = EXIT_FAILURE;
+		goto finish;
+	}
+
+	log_setup_kmod_log(ctx, verbose);
+
+	r = kmod_module_new_from_path(ctx, filename, &mod);
+	if (r < 0) {
+		ERR("could not load module %s: %s\n", filename, strerror(-r));
+		goto finish;
+	}
+
+	r = kmod_module_insert_module(mod, flags, opts);
+	if (r < 0)
+		ERR("could not insert module %s: %s\n", filename, mod_strerror(-r));
+
+	kmod_module_unref(mod);
+
+finish:
+	kmod_unref(ctx);
+	return r == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+static int do_insmod(int argc, char *argv[])
+{
 	const char *filename;
+	char dirname_buf[PATH_MAX];
 	char *opts = NULL;
 	int verbose = LOG_ERR;
 	int use_syslog = 0;
 	int c, r = 0;
-	const char *null_config = NULL;
 	unsigned int flags = 0;
 
 	while ((c = getopt_long(argc, argv, cmdopts_s, cmdopts, NULL)) != -1) {
@@ -115,33 +168,17 @@ static int do_insmod(int argc, char *argv[])
 	if (r < 0)
 		goto end;
 
-	ctx = kmod_new(NULL, &null_config);
-	if (!ctx) {
-		ERR("kmod_new() failed!\n");
-		r = EXIT_FAILURE;
+	r = get_module_dirname(dirname_buf, sizeof(dirname_buf),
+			       MODULE_DIRECTORY);
+	if (r)
 		goto end;
-	}
-
-	log_setup_kmod_log(ctx, verbose);
-
-	r = kmod_module_new_from_path(ctx, filename, &mod);
-	if (r < 0) {
-		ERR("could not load module %s: %s\n", filename, strerror(-r));
-		goto end;
-	}
-
-	r = kmod_module_insert_module(mod, flags, opts);
-	if (r < 0)
-		ERR("could not insert module %s: %s\n", filename, mod_strerror(-r));
-
-	kmod_module_unref(mod);
+	r = _do_insmod(dirname_buf, filename, flags, opts, verbose);
 
 end:
-	kmod_unref(ctx);
 	free(opts);
 
 	log_close();
-	return r == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+	return r;
 }
 
 const struct kmod_cmd kmod_cmd_compat_insmod = {
