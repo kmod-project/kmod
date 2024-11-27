8ebaf6eb06fffdac0432a074edddbcf8a9a05ef2
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2024-11-27 11:14:23 -0500

    tools/lsmod: add logic for MODULE_ALTERNATIVE_DIRECTORY
    If nothing is found in MODULE_DIRECTORY, perform the same exact steps
    but with MODULE_ALTERNATIVE_DIRECTORY.
    
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>

--- tools/lsmod.c
+++ tools/lsmod.c
@@ -153,11 +153,20 @@ static int do_lsmod(int argc, char *argv[])
 		goto done;
 	}
 
+	/* Try first with MODULE_DIRECTORY */
 	r = get_module_dirname(dirname_buf, sizeof(dirname_buf),
 			       MODULE_DIRECTORY);
-	if (r)
+	if (!r)
+		r = _do_lsmod(dirname_buf, verbose);
+	if (!r)
+		/* MODULE_DIRECTORY was succesful */
 		goto done;
-	r = _do_lsmod(dirname_buf, verbose);
+
+	/* If not found, look at MODULE_ALTERNATIVE_DIRECTORY */
+	r = get_module_dirname(dirname_buf, sizeof(dirname_buf),
+			       MODULE_ALTERNATIVE_DIRECTORY);
+	if (!r)
+		r = _do_lsmod(dirname_buf, verbose);
 
 done:
 	log_close();
