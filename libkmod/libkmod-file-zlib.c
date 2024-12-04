c22cce1fde80ac5f6a2a9bcb362dff36db4f4e70
Lucas De Marchi <lucas.de.marchi@gmail.com>
2024-12-04 01:19:32 -0600

    meson: Allow to set dlopen option for compression libraries
    Add a dlopen option that allows toggling what libraries libkmod should
    attempt to dlopen. If -Ddlopen=foo is passed, it means that library is
    required to build, regardless of -Dfoo=*. However that library will
    only be linked in if it's not set as dlopen.
    
    Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
    Link: https://github.com/kmod-project/kmod/pull/262

--- libkmod/libkmod-file-zlib.c
+++ libkmod/libkmod-file-zlib.c
@@ -3,8 +3,7 @@
  * Copyright © 2024 Intel Corporation
  */
 
-/* TODO: replace with build system define once supported */
-#define DLSYM_LOCALLY_ENABLED 0
+#define DLSYM_LOCALLY_ENABLED ENABLE_ZLIB_DLOPEN
 
 #include <errno.h>
 #include <stdio.h>
