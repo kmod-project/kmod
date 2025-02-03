19430f9e1e729b8a5d0e1031e873da42221058dc
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2025-02-03 08:04:21 -0500

    tools/lsmod: perform alternative directory lookup only if enabled -- can be squashed
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>

--- tools/lsmod.c
+++ tools/lsmod.c
@@ -162,11 +162,13 @@ static int do_lsmod(int argc, char *argv[])
 		/* MODULE_DIRECTORY was succesful */
 		goto done;
 
+	#if ENABLE_ALTERNATIVE_DIR
 	/* If not found, look at MODULE_ALTERNATIVE_DIRECTORY */
 	r = get_module_dirname(dirname_buf, sizeof(dirname_buf),
 			       MODULE_ALTERNATIVE_DIRECTORY);
 	if (!r)
 		r = _do_lsmod(dirname_buf, verbose);
+	#endif
 
 done:
 	log_close();
