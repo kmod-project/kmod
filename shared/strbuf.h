fadbbabf5dca65970301eec29b2314e83ef9ab6f
Tobias Stoeckmann <tobias@stoeckmann.org>
2025-02-24 20:03:05 +0100

    shared: drop strbuf_steal
    The only use case of strbuf_steal is gone. If the requirement for this
    function returns, we can add it again.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- shared/strbuf.h
+++ shared/strbuf.h
@@ -44,18 +44,6 @@ void strbuf_release(struct strbuf *buf);
 void strbuf_clear(struct strbuf *buf);
 
 /*
- * Return a copy as a C string, guaranteed to be nul-terminated. On success, the @buf
- * becomes invalid and shouldn't be used anymore, except for an (optional) call to
- * strbuf_release() which still does the right thing on an invalidated buffer. On failure,
- * NULL is returned and the buffer remains valid: strbuf_release() should be called.
- * Consider using _cleanup_strbuf_ attribute to release the buffer as needed.
- *
- * The copy may use the same underlying storage as the buffer and should be free'd later
- * with free().
- */
-char *strbuf_steal(struct strbuf *buf);
-
-/*
  * Return a C string owned by the buffer. It becomes an invalid
  * pointer if strbuf is changed. It may also not survive a return
  * from current function if it was initialized with stack space
