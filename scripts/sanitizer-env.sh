kmod-project/kmod

commit f5b4ff82f45af668cb4d62043184829b1e9621cb

Add support for clang sanitizers
By default clang uses static sanitizer libraries, which causes build and
test-time failures. Swap for the shared libasan which resolves both.

Note: meson tries to be helpful here, throwing a warning that we should
use -D b_lundef=false which is incorrect in our case.

Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Link: https://github.com/kmod-project/kmod/pull/172
Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

scripts/sanitizer-env.sh
@@ -2,7 +2,16 @@
 
 # set -euo pipefail # don't set these, since this script is sourced
 
-OUR_PRELOAD=$(gcc -print-file-name=libasan.so)
+if [[ ${CC-} == *gcc* ]]; then
+    OUR_PRELOAD=$("$CC" -print-file-name=libasan.so)
+elif [[ ${CC-} == *clang* ]]; then
+    OUR_PRELOAD=$("$CC" -print-file-name=libclang_rt.asan-x86_64.so)
+else
+    echo "Unknown compiler CC=\"${CC-}\" - gcc and clang are supported."
+    echo "Assuming \"gcc\", manually set the variable and retry if needed."
+    echo
+    OUR_PRELOAD=$(gcc -print-file-name=libasan.so)
+fi
 
 if test -n "$OUR_PRELOAD"; then
     # In some cases, like in Fedora, the file is a script which cannot be

meson.build
@@ -159,6 +159,17 @@ add_project_link_arguments(
   language : 'c'
 )
 
+# Clang as of v18, relies on statically linking the sanitizers. This causes two
+# distinct issues:
+#  - the shared library is underlinked, so the build fails
+#  - the modules (that we dlopen/ld_preload) are underlinked so the tests fail
+#
+# Force shared libasan (GCC defaults to shared and this toggle doesn't exist),
+# which combined with the LD_PRELOAD in our wrapper makes everything happy.
+if get_option('b_sanitize') != 'none' and cc.get_id() == 'clang'
+  add_project_arguments('-shared-libasan', language : 'c')
+  add_project_link_arguments('-shared-libasan', language : 'c')
+endif
 ################################################################################
 # Options
 ################################################################################

commit 9e1ee1296cabf1e445832fa9955764ba01b169dc

scripts/sanitizer-env.sh: remove incorrect $CC quotes
On Arch (at least) the shell chokes when the CC variable contains space,
for the given construct - $("$CC" foobar).

Since we variable is likely to contain the executable and arguments, the
quotation is wrong. Drop it and ultimately resolve the LD_PRELOAD
issues... That said, the tests still fail over here, segfault-ing in (or
due to) the LD_PRELOAD library init-modules.so.

Fixes: f5b4ff82 ("Add support for clang sanitizers")
Fixes: c7686797 ("scripts/sanitizer-env.sh: support new clang 19 DSO")
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

scripts/sanitizer-env.sh
@@ -3,12 +3,12 @@
 # set -euo pipefail # don't set these, since this script is sourced
 
 if [[ ${CC-} == *gcc* ]]; then
-    OUR_PRELOAD=$("$CC" -print-file-name=libasan.so)
+    OUR_PRELOAD=$($CC -print-file-name=libasan.so)
 elif [[ ${CC-} == *clang* ]]; then
     # With v19, the library lacks the CPU arch in its name
-    OUR_PRELOAD=$("$CC" -print-file-name=libclang_rt.asan.so)
+    OUR_PRELOAD=$($CC -print-file-name=libclang_rt.asan.so)
     if ! test -f "$OUR_PRELOAD"; then
-       OUR_PRELOAD=$("$CC" -print-file-name=libclang_rt.asan-x86_64.so)
+       OUR_PRELOAD=$($CC -print-file-name=libclang_rt.asan-x86_64.so)
     fi
 else
     cat <<- EOF >&2